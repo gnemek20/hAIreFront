e682bcaaabc84a7889521c01f2983b286a65241e
gnemek20 <gnemek20@gmail.com>
2026-02-18 10:35:54 +0900

    hotfix: change middleware auth logic

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 const middleware = (req: NextRequest) => {
   const auth = req.cookies.get("access_token");
 
+  // if (!auth) {
+  //   const currentSearchParams = new URL(req.url).searchParams;
+  //   const signinURL = new URL("/auth/signin", req.url);
+
+  //   currentSearchParams.forEach((value, key) => {
+  //     signinURL.searchParams.set(key, value);
+  //   });
+
+  //   return NextResponse.redirect(signinURL);
+  // }
   if (!auth) {
-    const currentSearchParams = new URL(req.url).searchParams;
-    const signinURL = new URL("/auth/signin", req.url);
-
-    currentSearchParams.forEach((value, key) => {
-      signinURL.searchParams.set(key, value);
-    });
-
-    return NextResponse.redirect(signinURL);
+    return NextResponse.redirect(
+      new URL("/signIn", req.url)
+    );
   }
 
   return NextResponse.next();
